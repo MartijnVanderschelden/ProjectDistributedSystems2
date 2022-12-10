773cf758121b755585897ad481cf5171a31cf073
aminkhatibi <aminkhatibi@icloud.com>
2022-12-10 14:31:30 +0100

    CapsulesList wordt geshuffeld vooraleer geflushed naar Matching

--- src/MixingProxy/MixingProxyImpl.java
+++ src/MixingProxy/MixingProxyImpl.java
@@ -82,6 +82,7 @@ public class MixingProxyImpl extends UnicastRemoteObject implements MixingProxy 
 
     @Override
     public void flush() throws RemoteException {
+        Collections.shuffle(capsulesList);
         for (int i = 0; i < capsulesList.size(); i++) {
             matchingService.retrieveCapsules(capsulesList.get(i));
         }
